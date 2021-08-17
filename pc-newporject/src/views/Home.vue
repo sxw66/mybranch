1b7be4a5c8b7c59f4d853badcf00aed8e8adbf38
sxw66 <79630277+sxw66@users.noreply.github.com>
2021-08-17 19:29:22 +0800

    Add files via upload

---
+++ pc-newporject/src/views/Home.vue
@@ -0,0 +1,88 @@
+<template>
+  <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+    <a-layout-sider v-model="collapsed" collapsible>
+      <h2 class="logo" style="color: white">营销工具系统</h2>
+      <a-menu theme="dark" :default-selected-keys="['1']" mode="inline">
+        <a-menu-item key="1">
+          <a-icon type="bank" />
+          <span>态势感知</span>
+        </a-menu-item>
+        <a-sub-menu key="2">
+          <template v-slot:title>
+            <span><a-icon type="user" />数据概览</span>
+          </template>
+          <a-menu-item key="3"> 商业概览 </a-menu-item>
+          <a-menu-item key="4"> 物业概览</a-menu-item>
+          <a-menu-item key="5"> 地产概览 </a-menu-item>
+          <a-menu-item key="6"> 商城概览 </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub1">
+          <template v-slot:title>
+            <span><a-icon type="user" />用户分类</span>
+          </template>
+          <a-menu-item key="3"> 用户标签 </a-menu-item>
+          <a-menu-item key="4"> 用户群分 </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template v-slot:title>
+            <span><a-icon type="team" /><span>标签管理</span></span>
+          </template>
+          <a-menu-item key="6"> 事件元 </a-menu-item>
+          <a-menu-item key="7"> 事件属性 </a-menu-item>
+          <a-menu-item key="8"> 用户属性 </a-menu-item>
+          <a-menu-item key="9"> 标签分组 </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <template v-slot:title>
+            <span><a-icon type="team" /><span>推送配置</span></span>
+          </template>
+          <a-menu-item key="61"> 信息推送 </a-menu-item>
+          <a-menu-item key="82"> 模板配置 </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub4">
+          <template v-slot:title>
+            <span><a-icon type="team" /><span>系统管理</span></span>
+          </template>
+          <a-menu-item key="61"> 用户管理 </a-menu-item>
+          <a-menu-item key="82"> 部门管理 </a-menu-item>
+          <a-menu-item key="82"> 权限管理 </a-menu-item>
+          <a-menu-item key="82"> 日志管理 </a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0" />
+      <a-layout-content style="margin: 0 16px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>User</a-breadcrumb-item>
+          <a-breadcrumb-item>Bill</a-breadcrumb-item>
+        </a-breadcrumb>
+        <div
+          :style="{ padding: '24px', background: '#fff', minHeight: '360px' }"
+        >
+          Bill is a cat.
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        Ant Design ©2018 Created by Ant UED
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      collapsed: true,
+    };
+  },
+};
+</script>
+
+<style>
+#components-layout-demo-side .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+</style>
---
+++ pc-newporject/src/views/Login.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="login-t">
+    <div class="login-c">
+      <div class="components-input-demo-presuffix">
+        <div class="login-title">欢迎使用</div>
+        <div class="login-title1">大会员系统</div>
+        <a-input  placeholder="请输入账号" />
+        <br />
+        <!-- <a-icon slot="prefix" type="user" /> -->
+        <a-input prefix="￥" placeholder="请输入密码" />
+        <a-button type="primary" block @click="sendlogin"> 登录 </a-button>
+        <div class="login-f">
+          <a-checkbox @change="onChange">记住密码</a-checkbox>
+          <a href="#">忘记密码?</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: "",
+    };
+  },
+  methods: {
+    emitEmpty() {
+      this.$refs.userNameInput.focus();
+      this.userName = "";
+    },
+    onChange(e) {
+      console.log(`checked = ${e.target.checked}`);
+    },
+    sendlogin() {
+      this.$router.push("/Home");
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+.login-t {
+  width: auto;
+  height: 100vh;
+  position: relative;
+  .ant-input-affix-wrapper {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-variant: tabular-nums;
+    list-style: none;
+    font-feature-settings: "tnum";
+    position: relative;
+    display: inline-flex;
+    border: 1px solid #d9d9d9;
+    border-radius: 2px;
+    padding: 4px 11px;
+    width: 100%;
+    text-align: start;
+    background-color: #fff;
+    background-image: none;
+    color: rgba(0, 0, 0, 0.85);
+    font-size: 14px;
+    line-height: 1.5715;
+    margin-top: 25px;
+  }
+  .ant-btn-primary {
+    color: #fff;
+    background: #1890ff;
+    border-color: #1890ff;
+    text-shadow: 0 -1px 0 rgb(0 0 0 / 12%);
+    box-shadow: 0 2px 0 rgb(0 0 0 / 5%);
+    margin-top: 25px;
+  }
+  .login-c {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    .login-f {
+      margin: 10px 0;
+      display: flex;
+      justify-content: space-between;
+    }
+    .login-title {
+      font-size: 48px;
+      color: #1890ff;
+      font-weight: bold;
+      display: flex;
+      justify-content: start;
+    }
+    .login-title1 {
+      font-size: 36px;
+      color: #1890ff;
+      display: flex;
+      justify-content: start;
+    }
+  }
+}
+</style>
